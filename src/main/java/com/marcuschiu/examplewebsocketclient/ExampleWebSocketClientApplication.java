5f4a71603c7a4974df93dca6b6b32ba6fbce8de2
Marcus Chiu <marcuschiu9@gmail.com>
2019-06-24 01:17:26 -0500

    added loop

--- src/main/java/com/marcuschiu/examplewebsocketclient/ExampleWebSocketClientApplication.java
+++ src/main/java/com/marcuschiu/examplewebsocketclient/ExampleWebSocketClientApplication.java
@@ -10,7 +10,7 @@ import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class ExampleWebSocketClientApplication implements CommandLineRunner {
@@ -23,12 +23,29 @@ public class ExampleWebSocketClientApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("entered command-line-runner");
 
+		StompSession session = generateWebSocketSession("ws://localhost:8080/gs-guide-websocket");
+
+		System.out.println("press any key to send message");
+		System.in.read();
+
+		System.out.println("Sending: Marcus Chiu");
+		Message msg = new Message();
+		Integer i = 0;
+		while (true) {
+			i++;
+			msg.setName(i.toString());
+			session.send("/app/hello", msg);
+		}
+	}
+
+	private StompSession generateWebSocketSession(String url) throws ExecutionException, InterruptedException {
 		// https://stackoverflow.com/questions/30413380/websocketstompclient-wont-connect-to-sockjs-endpoint
+		StompSession session;
 
 		// OPTION 1
 		WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
 		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-		StompSession session = stompClient.connect("ws://localhost:8080/gs-guide-websocket", new MyStompSessionHandler()).get();
+		session = stompClient.connect(url, new MyStompSessionHandler()).get();
 
 		// OPTION 2
 //		List<Transport> transports = new ArrayList<>(1);
@@ -36,7 +53,7 @@ public class ExampleWebSocketClientApplication implements CommandLineRunner {
 //		WebSocketClient transport = new SockJsClient(transports);
 //		WebSocketStompClient stompClient = new WebSocketStompClient(transport);
 //		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-//		StompSession session = stompClient.connect("ws://localhost:8080/gs-guide-websocket", new MyStompSessionHandler()).get();
+//		session = stompClient.connect("ws://localhost:8080/gs-guide-websocket", new MyStompSessionHandler()).get();
 //		session.subscribe("/topic/greetings", new StompFrameHandler() {
 //			@Override
 //			public Type getPayloadType(StompHeaders headers) {
@@ -51,14 +68,6 @@ public class ExampleWebSocketClientApplication implements CommandLineRunner {
 //			}
 //		});
 
-		while (true) {
-			System.out.println("press any key to send message");
-			System.in.read();
-
-			System.out.println("Sending: Marcus Chiu");
-			Message msg = new Message();
-			msg.setName("Marcus Chiu");
-			session.send("/app/hello", msg);
-		}
+		return session;
 	}
 }
